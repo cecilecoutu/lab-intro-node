d945d74adab0dbaaf577eadf6a2ae8e14d4a2441
your_github_username <your_github_email>
2024-01-08 21:31:08 +0100

    done

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    (this.items = []), (this.length = this.items.length);
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item), this.item.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
   get(pos) {}
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error(
+        "EmptyList: Cannot find the maximum value in an empty list."
+      );
+    }
 
-  min() {}
+    return this.items[this.length - 1];
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error("Empty SortedList");
+    }
+    return this.items[0];
+  }
 
   sum() {}
 
